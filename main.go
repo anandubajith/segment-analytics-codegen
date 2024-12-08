6af6f80a836ca0dbba71c7bd23d2ed4a2da9fed7
Anandu B Ajith <reach2anandu@gmail.com>
2024-12-08 21:20:52 +0530

    tweak: use file inline for now

--- main.go
+++ main.go
@@ -25,6 +25,43 @@ type Spec struct {
 	Events []EventSpec `yaml:"events"`
 }
 
+const codeTemplate = `package analytics
+
+import (
+	"fmt"
+	"github.com/segmentio/analytics-go"
+)
+
+
+type AnalyticsClient struct {
+	client analytics.Client
+}
+
+func NewAnalyticsClient(writeKey string) *AnalyticsClient {
+	client := analytics.New(writeKey)
+	return &AnalyticsClient{client}
+}
+
+func (c *AnalyticsClient) Close() {
+	c.client.Close()
+}
+
+{{range .Events}}
+type {{.Name | Title}}EventProps struct {
+    {{range .Properties}}{{.Name | Title}} {{.Type | GoType}}
+    {{end}}
+}
+
+func (c *AnalyticsClient) Track{{.Name | Title}}Event(properties {{.Name | Title}}EventProps) {
+    fmt.Printf("Tracking {{.Name}} event: %+v\n", properties)
+
+		 c.client.Enqueue(analytics.Track{
+        Event:  "{{.Name}}",
+				Properties: analytics.NewProperties(){{range .Properties}}.Set("{{.Name }}", properties.{{.Name | Title}}){{end}},
+    })
+}
+{{end}}`
+
 func goType(yamlType string) string {
 	switch yamlType {
 	case "string":
@@ -44,12 +81,7 @@ func main() {
 		panic("Usage: main.go <input.yaml> <output.go>")
 	}
 	inputFile := os.Args[1]
-	codeTemplate, err := os.ReadFile("template.go.tmpl")
 	outputFile := os.Args[2]
-
-	if err != nil {
-		panic(err)
-	}
 
 	data, err := os.ReadFile(inputFile)
 	if err != nil {
